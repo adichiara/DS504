3df2db74a611f4483fae7c1cddb1d8f55db83d25
adichiara <adichiara@users.noreply.github.com>
2020-10-29 15:15:52 -0400

    Update: README.md article_df.csv face_df.csv notebooks source_data.csv

---
+++ notebooks/facial_expression_recognition.py
@@ -0,0 +1,88 @@
+# Databricks notebook source
+import logging 
+logging.getLogger("py4j").setLevel(logging.ERROR)
+
+
+# COMMAND ----------
+
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+!pip install opencv-python
+import cv2
+
+!pip install requests
+!pip install urllib
+
+!pip install cmake
+!pip install boost
+!pip install boost-python --with-python3
+
+!pip install dlib
+
+!pip install face_recognition
+!pip install fer
+
+import requests
+import urllib
+from urllib.parse import urlparse
+import dlib
+import face_recognition
+from fer import FER
+import datetime
+
+
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+urllib.request.install_opener(opener)
+
+
+spark
+
+# COMMAND ----------
+
+face_df = pd.read_csv('/dbfs/saved_df/face_df.csv', header=0, index_col=0)
+
+# COMMAND ----------
+
+face_np = np.load('/dbfs/faces/face_np.npy', allow_pickle=True)
+
+# COMMAND ----------
+
+
+detector = FER(mtcnn=True)
+
+for i,x in face_df.iterrows():
+    
+    face = face_np[i]
+    detect_dict = detector.detect_emotions(face)
+    
+#     plt.imshow(face)
+#     title = str(detect_dict)
+#     plt.title(title)
+#     plt.show()
+        
+    if (bool(detect_dict)):
+        face_df.loc[i,'emotions'] = True
+        face_df.loc[i,'happy'] = detect_dict[0]['emotions']['happy']
+        face_df.loc[i,'sad'] = detect_dict[0]['emotions']['sad']
+        face_df.loc[i,'angry'] = detect_dict[0]['emotions']['angry']
+        face_df.loc[i,'fear'] = detect_dict[0]['emotions']['fear']
+        face_df.loc[i,'surprise'] = detect_dict[0]['emotions']['surprise']
+        face_df.loc[i,'disgust'] = detect_dict[0]['emotions']['disgust']
+        face_df.loc[i,'neutral'] = detect_dict[0]['emotions']['neutral']
+    else:
+        face_df.loc[i,'emotions'] = False
+
+
+
+# COMMAND ----------
+
+face_df.loc[face_df.emotions==False,:].index
+
+# COMMAND ----------
+
+face_df.to_csv('/dbfs/saved_df/face_df.csv')
