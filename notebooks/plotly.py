48354ba2cdee8c9200f811762b8dd00ff0a49029
adichiara <adichiara@users.noreply.github.com>
2020-10-29 13:01:53 -0400

    Update: README.md article_df.csv face_df.csv notebooks source_data.csv

--- notebooks/plotly.py
+++ notebooks/plotly.py
@@ -16,6 +16,8 @@ from plotly.subplots import make_subplots
 import skimage
 from skimage import io
 
+from datetime import datetime
+
 chart_studio.tools.set_credentials_file(username='adamjd', 
                                         api_key='1piYaG7ohT90yKxLEjL7')
 
@@ -70,6 +72,13 @@ fer_df.loc[fer_df['top_emotion']=='neutral', 'top_neutral'] = 1
 
 # COMMAND ----------
 
+timestamp = datetime.today()
+timestamp_str = "Updated at: " + str(timestamp.month) + "/" + str(timestamp.day) + "/" + str(timestamp.year) + " " + str(timestamp.hour) + ":" + str(timestamp.minute) + " UTC"
+
+
+
+# COMMAND ----------
+
 
 # SAVED AS percent-by-source-bias-grid
 
@@ -78,9 +87,9 @@ df1 = fer_df.groupby(['source_bias','name'], as_index=False).sum()
 df1['happy_pct'] = df1.top_happy/df1.emotions
 df1['sad_pct'] = df1.top_sad/df1.emotions
 df1['angry_pct'] = df1.top_angry/df1.emotions
-# df1['fear_pct'] = df1.top_fear/df1.emotions
-# df1['surprise_pct'] = df1.top_surprise/df1.emotions
-# df1['disgust_pct'] = df1.top_disgust/df1.emotions
+df1['fear_pct'] = df1.top_fear/df1.emotions
+df1['surprise_pct'] = df1.top_surprise/df1.emotions
+df1['disgust_pct'] = df1.top_disgust/df1.emotions
 df1['neutral_pct'] = df1.neutral/df1.emotions
 
 
@@ -89,9 +98,9 @@ df1 = pd.melt(df1,
               value_vars=['happy_pct', 
                    'sad_pct', 
                    'angry_pct', 
-#                    'fear_pct', 
-#                    'surprise_pct', 
-#                    'disgust_pct', 
+                   'fear_pct', 
+                   'surprise_pct', 
+                   'disgust_pct', 
                    'neutral_pct'])
 
 df1 = df1.groupby(['source_bias','name','variable'], as_index=False).mean()
@@ -99,28 +108,32 @@ df1 = df1.groupby(['source_bias','name','variable'], as_index=False).mean()
 df1.sort_values(['name'], ascending=False, inplace=True)
 
 df1.loc[df1.variable=='happy_pct', 'variable'] = 'Happy'
-df1.loc[df1.variable=='angry_pct', 'variable'] = 'Angry'
-df1.loc[df1.variable=='neutral_pct', 'variable'] = 'Neutral'
 df1.loc[df1.variable=='sad_pct', 'variable'] = 'Sad'
+df1.loc[df1.variable=='angry_pct', 'variable'] = 'Angry'
+df1.loc[df1.variable=='fear_pct', 'variable'] = 'Fear'
+df1.loc[df1.variable=='surprise_pct', 'variable'] = 'Surprise'
+df1.loc[df1.variable=='disgust_pct', 'variable'] = 'Disgust'
+df1.loc[df1.variable=='neutral_pct', 'variable'] = 'Neutral'
 
 df1.value = np.round(df1.value*100,0)
 
 fig = px.bar(df1, 
              x="name", 
              y="value",
-             color='name', 
-             facet_col='source_bias',
-             facet_row='variable',
+             color='variable', 
+             text='value',
+             facet_col='variable',
+             facet_row='source_bias',
              height=600, width=1000,
-             labels=dict(name="", value="%"),
+             labels=dict(name="", value="%", variable=""),
 #             template="simple_white",
-             title='What percent of the photos from news sources with each bias rating show <br>Joe Biden and Donald Trump with either Happy, Sad, Angry, or Neutral facial expressions?',
+             title='What percent of the photos from news sources (by bias rating) contain facial expressions?<br>.  ' + timestamp_str,
              category_orders={"source_bias":['Left',
                                              'Lean Left',
                                              'Center',
                                              'Lean Right', 
                                              'Right'],
-                             "variable":['Happy','Sad','Angry','Neutral']}
+                             "variable":['Happy','Sad','Angry','Fear','Surprise','Disgust','Neutral']}
             )
 
 
@@ -135,7 +148,7 @@ py.iplot(fig, filename='percent-by-source-bias-grid')
 
 # COMMAND ----------
 
-
+date_list = np.sort(fer_df['date'].unique())
 
 fig = px.bar(fer_df, 
              x="date", 
@@ -144,12 +157,13 @@ fig = px.bar(fer_df,
              height=600, width=1400,
              labels=dict(name="", value="%"),
 #              template="simple_white",
-             title='Number of news articles in search query containing images of Donald Trump and Joe Biden (from past week).',
+             title='Number of news articles in search query containing images of Donald Trump and Joe Biden (from past week).<br>' + timestamp_str,
              category_orders={"source_bias":['Left',
                                              'Lean Left',
                                              'Center',
                                              'Lean Right', 
-                                             'Right']}
+                                             'Right'],
+                             "date":date_list}
             )
 
  
@@ -190,7 +204,7 @@ fig = px.bar(df1,
              text='top_emotion_sum',
              facet_col='top_emotion',    
              height=400, width=1200,
-             title="How many face images returned by the search query were classified for each facial expression?",
+             title="How many face images returned by the search query were classified for each facial expression?<br>" + timestamp_str,
              labels=dict(name="", top_emotion_sum="Number of face images classified", top_emotion="Predicted Facial Expression"),     
 #              template="simple_white",
              category_orders={"top_emotion":['Happy','Sad','Angry','Fear','Surprise','Disgust','Neutral']}
@@ -206,14 +220,13 @@ py.iplot(fig, filename='facial-expression-totals-barchart')
 # COMMAND ----------
 
 
-df1 = fer_df
-df1.sort_values(['name'], ascending=False, inplace=True)
+df1 = article_totals
 
 fig = px.bar(df1, 
-             y='source',
+             y='source_name',
              x='num_articles',             
-             title='How many total news articles were collected from each news source?',
-             labels=dict(name="", source="News Source", count="Total Articles Collected in Latest Query"),     
+             title='How many total news articles were collected from each news source?<br>' + timestamp_str,
+             labels=dict(source_name="News Source", num_articles="Total Articles Collected in Latest Query"),     
              height=600, width=600)
 
 fig.update_layout(yaxis={'categoryorder':'total ascending'})
@@ -224,17 +237,25 @@ py.iplot(fig, filename='total-articles-by-source')
 # COMMAND ----------
 
 
+
+# COMMAND ----------
+
+
 df1 = fer_df
 df1.sort_values(['name'], ascending=False, inplace=True)
+
+num_articles_order = list(article_totals.sort_values('num_articles', ascending=False)['source_name'])
+
 
 fig = px.bar(df1, 
              y='source',
              color='name',
-             title='How many images collected from each news source contained faces of<br> Donald Trump and Joe Biden, according to the model?',
+             title='How many faces were detected in the images collected from each news source<br>'  + timestamp_str,
              labels=dict(name="", source="News Source", count="Number of Faces Detected in Collected Articles"),     
+             category_orders={'source':num_articles_order},
              height=600, width=700)
 
-fig.update_layout(yaxis={'categoryorder':'total ascending'})
+# fig.update_layout(yaxis={'categoryorder':[num_articles_order]})
 fig.show()
 py.iplot(fig, filename='total-faces-detected-by-source')
 
@@ -267,7 +288,7 @@ fig = px.box(df1,
              x='variable',
              color='variable',
              height=600, width=700,
-             title='What were the distributions of probabilities returned by the facial recognition<br>model for each type of facial expression?',
+             title='What were the distributions of probabilities returned by the facial recognition<br>model for each type of facial expression?   ' + timestamp_str,
              labels=dict(name="", variable="", value="Predicted Probability of Emotion"),
              category_orders={"variable":['Happy','Sad','Angry','Fear','Surprise','Disgust','Neutral']})
                      
@@ -294,59 +315,16 @@ fig = px.box(df1,
              x='top_emotion',
 #              color='top_emotion',
              height=600, width=700,
-             title='For the highest probability expression for each face image (the "top emotion"),<br> what were the distributions of those probabilities?',
+             title='What were the distributions for the highest predicted probability expression<br>for each face image (i.e., the "top emotion")?  ' + timestamp_str,
              labels=dict(top_emotion_prob="Probabilities of Top Expression for Each Face", top_emotion="Top Emotion"),
              category_orders={"top_emotion":['Happy','Sad','Angry','Fear','Surprise','Disgust','Neutral']})
 
 
 fig.update_yaxes(range=[0,1])
 
-fig.show()
 py.iplot(fig, filename='top-emotion-probability-boxplots')
-
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-import skimage
-from skimage import io
-
-urls = []
-for i in ['Happy','Sad','Angry','Fear','Surprise','Disgust','Neutral']:
-
-    df1 = fer_df.loc[fer_df.top_emotion == i,:]
-    df1 = df1.sort_values(['top_emotion_prob'], ascending=False)
-    df1 = df1.reset_index(drop=True)
-    print(df1.loc[0,'urlToImage'])
-    urls.append(df1.loc[0,'urlToImage'])
-
-
-
-fig = make_subplots(rows=1, cols=2)
-
-fig.add_trace(
-    go.imshow(io.imread(urls[0])),
-    row=1, col=1
-)
-
-fig.add_trace(
-    go.imshow(io.imread(urls[1])),
-    row=1, col=2
-)
-
-fig.update_layout(height=600, width=800, title_text="Side By Side Subplots")
 fig.show()
 
-# COMMAND ----------
-
-face_df
-
-# COMMAND ----------
-
-face_df['disgust'].value_counts()
 
 # COMMAND ----------
 
@@ -355,7 +333,7 @@ face_df['disgust'].value_counts()
 # COMMAND ----------
 
 
-emotion = 'surprise'
+emotion = 'happy'
 
 row = face_df[emotion].idxmax()
 url = face_df.loc[row,'urlToImage']
@@ -373,6 +351,71 @@ fig.add_trace(go.Image(z=img_face), 1, 2)
 
 fig.update_xaxes(showticklabels=False, showgrid=False)
 fig.update_yaxes(showticklabels=False, showgrid=False)
+fig.update_layout(title_text= emotion + " - Image in latest query with highest predicted probability for this expression.   " + timestamp_str)
 
+
+filename_str = 'show-image-with-highest-probability-' + emotion
+# py.iplot(fig, filename=filename_str)
 fig.show()
-    
+
+
+
+# COMMAND ----------
+
+
+emotion = 'sad'
+
+row = face_df[emotion].idxmax()
+url = face_df.loc[row,'urlToImage']
+img_all = io.imread(url)
+img_face = face_np[row]
+prob = np.round(face_df.loc[row,emotion],2)
+title = emotion + ": " + str(prob) + " probability"
+
+fig = make_subplots(
+    rows=1, cols=2,
+    subplot_titles=(url,title))
+
+fig.add_trace(go.Image(z=img_all), 1, 1)
+fig.add_trace(go.Image(z=img_face), 1, 2)
+
+fig.update_xaxes(showticklabels=False, showgrid=False)
+fig.update_yaxes(showticklabels=False, showgrid=False)
+fig.update_layout(title_text= emotion + " - Image in latest query with highest predicted probability for this expression.   " + timestamp_str)
+
+
+filename_str = 'show-image-with-highest-probability-' + emotion
+# py.iplot(fig, filename=filename_str)
+fig.show()
+
+
+
+
+# COMMAND ----------
+
+
+emotion = 'angry'
+
+row = face_df[emotion].idxmax()
+url = face_df.loc[row,'urlToImage']
+img_all = io.imread(url)
+img_face = face_np[row]
+prob = np.round(face_df.loc[row,emotion],2)
+title = emotion + ": " + str(prob) + " probability"
+
+fig = make_subplots(
+    rows=1, cols=2,
+    subplot_titles=(url,title))
+
+fig.add_trace(go.Image(z=img_all), 1, 1)
+fig.add_trace(go.Image(z=img_face), 1, 2)
+
+fig.update_xaxes(showticklabels=False, showgrid=False)
+fig.update_yaxes(showticklabels=False, showgrid=False)
+fig.update_layout(title_text= emotion + " - Image in latest query with highest predicted probability for this expression.   " + timestamp_str)
+
+
+filename_str = 'show-image-with-highest-probability-' + emotion
+# py.iplot(fig, filename=filename_str)
+fig.show()
+
